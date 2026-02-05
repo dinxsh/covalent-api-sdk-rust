f3c6182847c43a63691e2f7fe9025c13cc38d08e
dinxsh <talwadkerdinesh@gmail.com>
2026-02-05 06:33:15 +0530

    chore: added tickers for chains

---
+++ src/chains.rs
@@ -0,0 +1,67 @@
+//! Chain constants for commonly used blockchain networks in GoldRush API.
+
+/// Ethereum networks
+pub mod ethereum {
+    /// Ethereum mainnet
+    pub const MAINNET: &str = "eth-mainnet";
+    
+    /// Ethereum Goerli testnet
+    pub const GOERLI: &str = "eth-goerli";
+    
+    /// Ethereum Sepolia testnet  
+    pub const SEPOLIA: &str = "eth-sepolia";
+}
+
+/// Polygon networks
+pub mod polygon {
+    /// Polygon mainnet
+    pub const MAINNET: &str = "matic-mainnet";
+    
+    /// Polygon Mumbai testnet
+    pub const MUMBAI: &str = "matic-mumbai";
+}
+
+/// Binance Smart Chain networks
+pub mod bsc {
+    /// BSC mainnet
+    pub const MAINNET: &str = "bsc-mainnet";
+    
+    /// BSC testnet
+    pub const TESTNET: &str = "bsc-testnet";
+}
+
+/// Avalanche networks
+pub mod avalanche {
+    /// Avalanche mainnet
+    pub const MAINNET: &str = "avalanche-mainnet";
+    
+    /// Avalanche Fuji testnet
+    pub const FUJI: &str = "avalanche-fuji";
+}
+
+/// Arbitrum networks
+pub mod arbitrum {
+    /// Arbitrum One mainnet
+    pub const MAINNET: &str = "arbitrum-mainnet";
+    
+    /// Arbitrum Goerli testnet
+    pub const GOERLI: &str = "arbitrum-goerli";
+}
+
+/// Optimism networks
+pub mod optimism {
+    /// Optimism mainnet
+    pub const MAINNET: &str = "optimism-mainnet";
+    
+    /// Optimism Goerli testnet
+    pub const GOERLI: &str = "optimism-goerli";
+}
+
+/// Base networks
+pub mod base {
+    /// Base mainnet
+    pub const MAINNET: &str = "base-mainnet";
+    
+    /// Base Goerli testnet
+    pub const GOERLI: &str = "base-goerli";
+}
